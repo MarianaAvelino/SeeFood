960f1aa4cc2f40d18a59b934da52a0d9fd0d5271
user132375 <user132375@eu245.local>
2017-11-30 11:40:00 +0000

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by user132375 on 11/30/17.
+//  Copyright © 2017 user132375. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
